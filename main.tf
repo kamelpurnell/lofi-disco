e20dae5372d14feb41a1fcfc11adc6b8a07c791e
kamelpurnell <59943448+kamelpurnell@users.noreply.github.com>
2020-01-15 21:32:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "default" {
   ami                    = "ami-021acbdb89706aa89"
   count                  = 1
   key_name               = "terraform"
-  vpc_security_group_ids = [aws_security_group.default.id]
+  vpc_security_group_ids = [aws_security_group.terraform-default-sg.id]
   source_dest_check      = false
   instance_type          = "t2.micro"
 
